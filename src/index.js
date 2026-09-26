aa7be0c681acee3259d6ddc2ba69638538dce033
burnmaker78-cmd <burnmaker78@gmail.com>
2026-09-25 21:37:43 -0400

    Simplify fetch handler to return assets directly

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-
-    if (url.pathname === "/" || url.pathname === "/index.svg") {
-      return env.ASSETS.fetch(
-        new Request(new URL("/index.svg", request.url))
-      );
-    }
-
-    return new Response("Not Found", {
-      status: 404
-    });
+    return env.ASSETS.fetch(request);
   }
 };
